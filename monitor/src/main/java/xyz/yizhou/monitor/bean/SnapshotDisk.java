df9dfced33c6a359e6381632a5315ab557debe8d
hyizhou <hyizhou.outlook.com>
2021-10-13 18:05:04 +0800

    磁盘快照类完善

--- monitor/src/main/java/xyz/yizhou/monitor/bean/SnapshotDisk.java
+++ monitor/src/main/java/xyz/yizhou/monitor/bean/SnapshotDisk.java
@@ -11,10 +11,29 @@ public class SnapshotDisk {
 
     /** 磁盘总大小 */
     private long size;
+    /** 使用空间大小 */
+    private long useSize;
+    /** 读取速度，单位byte/s */
+    private double readSpeed;
+    /** 写入速度，单位byte/s */
+    private double writeSpeed;
 
+    /**
+     * 由自动任务调用，一秒钟调用一次
+     * @param disk 硬盘对象，代表一块硬盘
+     * @return 本类的实例
+     */
     public static SnapshotDisk snapshot(HWDiskStore disk){
+        long lastReadSize = disk.getReadBytes();
+        long lastWriteSize = disk.getWriteBytes();
+        // 手动刷新信息
+        disk.updateDiskStats();
         SnapshotDisk snapshot = new SnapshotDisk();
         snapshot.size = disk.getSize();
+        long readSize = disk.getReadBytes();
+        long writeSize = disk.getWriteBytes();
+        snapshot.readSpeed = (double) (readSize - lastReadSize);
+        snapshot.writeSpeed = (double) (writeSize - lastWriteSize);
         return snapshot;
     }
 }
