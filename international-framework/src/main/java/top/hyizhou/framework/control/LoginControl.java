206a095191246b46e915343d6e64fc69d79a7e08
hyizhou <hyizhou@outlook.com>
2021-12-23 17:58:57 +0800

    增加登录页面

--- international-framework/src/main/java/top/hyizhou/framework/control/LoginControl.java
+++ international-framework/src/main/java/top/hyizhou/framework/control/LoginControl.java
@@ -1,9 +1,19 @@
 package top.hyizhou.framework.control;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 登录功能控制器
  * @author huanggc
  * @date 2021/12/23 9:57
  */
+@RequestMapping()
+@Controller
 public class LoginControl {
+    @PostMapping("/login")
+    public String login(String userName, String password){
+        return null;
+    }
 }
