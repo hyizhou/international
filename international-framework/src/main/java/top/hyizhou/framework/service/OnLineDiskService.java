8754d95e3a3e6978b0fa5c0b99c10dccac03cb20
hyizhou <hyizhou@outlook.com>
2022-02-15 21:47:13 +0800

    完成云空间初始化方法

--- international-framework/src/main/java/top/hyizhou/framework/service/OnLineDiskService.java
+++ international-framework/src/main/java/top/hyizhou/framework/service/OnLineDiskService.java
@@ -8,6 +8,8 @@ import top.hyizhou.framework.entity.User;
 import top.hyizhou.framework.mapper.OnLineDiskMapper;
 import top.hyizhou.framework.mapper.SharedMapper;
 import top.hyizhou.framework.pojo.OnLineDisk;
+import top.hyizhou.framework.utils.DiskUtil;
+import java.io.File;
 
 /**
  * 网盘服务
@@ -25,7 +27,7 @@ public class OnLineDiskService {
     @Autowired
     private OnLineDiskMapper onLineDiskMapper;
     /** 实体目录路径 */
-    private String onLineDiskDir;
+    private String createPath;
 
     /**
      * 初始化工作，用户创建网盘时调用的方法，如创建网盘信息表记录，创建物理盘中文件目录
@@ -34,16 +36,31 @@ public class OnLineDiskService {
     public void create(User user){
         log.debug("用户网盘初始化开始");
         Integer userId = user.getId();
+        String accountName = user.getAccountName();
         // 验证用户是否以存在网盘
         OnLineDisk onLineDiskInfo = onLineDiskMapper.findById(userId);
         if (onLineDiskInfo != null){
-            log.info("用户网盘初始化时发现以存在网盘信息");
+            log.info("用户网盘初始化时发现已存在网盘");
             return;
         }
-        // todo
-        // 判断空间是否充足
+        // 判断空间是否充足，大于1G
+        if (!DiskUtil.hasEnoughSpace(new File(createPath), 1073741824)) {
+            log.error("用户网盘初始化失败--空间不足");
+            return;
+        }
         // 新建用户网盘目录
+        String userDir = createPath + File.separator + accountName;
+        if (!new File(userDir).mkdirs()) {
+            log.error("用户创建网盘初始化失败--创建目录失败");
+        }
         // 信息记入表
+        OnLineDisk onLineDisk = new OnLineDisk();
+        onLineDisk.setUserId(userId);
+        onLineDisk.setDirName(userDir);
+        onLineDisk.setAllSize(DefaultConf.allSize);
+        onLineDisk.setUseSize(0L);
+        onLineDisk.setShutoff(false);
+        onLineDiskMapper.insert(onLineDisk);
         log.debug("用户网盘初始化结束");
     }
 
@@ -70,4 +87,12 @@ public class OnLineDiskService {
      * todo 分享文件
      */
     public void shared(){}
+
+    /**
+     * 写一些配置
+     */
+    private static class DefaultConf{
+        /** 新建的网盘默认分配1G空间 */
+        public static long allSize = 1073741824;
+    }
 }
--- international-framework/src/main/java/top/hyizhou/framework/utils/DiskUtil.java
+++ international-framework/src/main/java/top/hyizhou/framework/utils/DiskUtil.java
@@ -1,5 +1,7 @@
 package top.hyizhou.framework.utils;
 
+import java.io.File;
+
 /**
  * 硬盘工具类
  * @author hyizhou
@@ -8,9 +10,20 @@ package top.hyizhou.framework.utils;
 public class DiskUtil {
     /**
      * 获得计算机盘符
-     * @return 盘符列表，若是linux
+     * @return 盘符列表
      */
-    public String[] getPartition(){
-        return null;
+    public static File[] getPartition(){
+        return File.listRoots();
+    }
+
+    /**
+     * 判断文件所在盘符是否有足够空间
+     * @param file 文件对象
+     * @param space 指定的空间数，单位byte
+     * @return 若剩余空间少于指定大小，则会返回false
+     */
+    public static boolean hasEnoughSpace(File file, long space){
+        long freeSpace = file.getFreeSpace();
+        return freeSpace >= space;
     }
 }
